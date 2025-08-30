2ffb2d36e1ceddd5760d365bc0365f36e73d7315
karlssoftware <baumgartner.carlo@gmail.com>
2025-08-30 16:13:01 +0200

    upadated url

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css',
+})
+export class AppComponent {
+ 
+}
